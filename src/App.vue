d90910f841c42be6f20222b1989d10ffe91f698f
Jeric Epon <jericepon@gmail.com>
2025-03-18 16:10:08 +0800

    initialize Vue 3 project with TypeScript, Vite, and Tailwind CSS

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="grid grid-cols-3 gap-4">
+    <div class="bg-gray-400 w-full h-28"></div>
+    <div class="bg-gray-400 w-full h-28"></div>
+    <div class="bg-gray-400 w-full h-28"></div>
+    <div class="bg-gray-400 w-full h-28"></div>
+  </div>
+</template>
+
+<style scoped></style>
